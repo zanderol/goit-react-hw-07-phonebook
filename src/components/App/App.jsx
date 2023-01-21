ac24c0d89a5fe4167ad2ccae8465a16bfbb3fe6c
ZanderOl <olexandruk@gmail.com>
2023-01-21 15:56:48 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,18 @@
+import { ThemeProvider } from 'styled-components';
 import { useSelector } from 'react-redux';
+import { getContacts } from 'redux/selectors';
+import ContactsForm from 'components/Form';
+import ThemeBtn from 'components/ThemeBtn';
+import ContactList from 'components/ListContact';
+import Filter from 'components/Filter';
+import {
+  Container,
+  ContainerList,
+  ListTitle,
+  FormTitle,
+  ListIsEmpty,
+  Global,
+} from './App.styled';
 
 const App = () => {
   const theme = useSelector(state => state.theme);
@@ -7,7 +21,25 @@ const App = () => {
 
   return (
     <>
-      <Container></Container>
+      <Container>
+        <ThemeProvider theme={theme}>
+          <Global />
+          <FormTitle>Phonebook</FormTitle>
+          <ThemeBtn />
+          <ContactsForm></ContactsForm>
+          <ContainerList>
+            <ListTitle>Contacts</ListTitle>
+            {contacts.length > 0 ? (
+              <Filter></Filter>
+            ) : (
+              <ListIsEmpty>
+                Your list is empty... Please add a new contact
+              </ListIsEmpty>
+            )}
+            <ContactList></ContactList>
+          </ContainerList>
+        </ThemeProvider>
+      </Container>
     </>
   );
 };
