36006229e2c55d718b2fc94c9d4569df901ec4aa
ZanderOl <olexandruk@gmail.com>
2023-01-21 15:39:59 +0200

    Create selectors.js

---
+++ src/redux/selectors.js
@@ -0,0 +1,7 @@
+const getContacts = state => state.contacts.contacts;
+
+const getByFilter = state => state.filter.filter;
+
+const getTheme = state => state.theme;
+
+export { getContacts, getByFilter, getTheme };
