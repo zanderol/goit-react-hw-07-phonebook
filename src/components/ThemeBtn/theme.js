8ccbec1efe597cd24bf1ffc4ffe20bd85f7d4d98
ZanderOl <olexandruk@gmail.com>
2023-01-22 18:46:51 +0200

    Create theme.js

---
+++ src/components/ThemeBtn/theme.js
@@ -0,0 +1,16 @@
+const lightTheme = {
+  body: '#bcc2f0',
+  text: '#000000',
+  main: '#0385ff',
+  input: '#cbffe1',
+  active: '#8f8c2b',
+};
+const darkTheme = {
+  body: '#1c1c1e',
+  text: '#ffffff',
+  main: '#030309',
+  input: '#27302b',
+  active: '#4e85e3',
+};
+
+export { lightTheme, darkTheme };
