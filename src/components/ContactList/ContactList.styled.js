2b44b7d47e332bca25c252b8957400e507ea70a9
ZanderOl <olexandruk@gmail.com>
2023-01-22 17:00:52 +0200

    Update ContactList.styled.js

--- src/components/ContactList/ContactList.styled.js
+++ src/components/ContactList/ContactList.styled.js
@@ -25,28 +25,104 @@ const ListItem = styled.li`
     max-width: 600px;
   }
 `;
+
+const Number = styled.span`
+  font-style: italic;
+  margin-left: 5px;
+  font-size: 10px;
+  @media screen and (min-width: 768px) {
+    font-size: 16px;
+    margin-left: 10px;
+  }
+`;
+const animCall = keyframes`
+0% {transform: rotate(0deg)};
+50% {transform: rotate(-30deg)};
+100% {transform: rotate(0deg)};
+`;
+const animDelete = keyframes`
+0% {transform: scale(1)};
+50% {transform: scale(1.1)};
+100% {transform: scale(1)};
+`;
+const CallTo = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: auto;
+  width: 20px;
+  height: 20px;
+  border: none;
+  cursor: pointer;
+  color: ${props => props.theme.text};
+  @media screen and (min-width: 768px) {
+    width: 30px;
+    height: 30px;
+  }
+  & > svg {
+    transition: all 0.3s;
+  }
+  &:hover > svg {
+    fill: green;
+    animation-name: ${animCall};
+    animation-duration: 1s;
+    animation-iteration-count: infinite;
+  }
+`;
+const MailTo = styled.a`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-left: 5px;
+  width: 20px;
+  height: 20px;
+  border: none;
+  cursor: pointer;
+  color: ${props => props.theme.text};
+  @media screen and (min-width: 768px) {
+    width: 30px;
+    height: 30px;
+  }
+  & > svg {
+    transition: all 0.3s;
+  }
+  &:hover > svg {
+    fill: orange;
+    animation-name: ${animDelete};
+    animation-duration: 1s;
+    animation-iteration-count: infinite;
+  }
+`;
+
 const DeleteButton = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 30px;
-  height: 30px;
-  background-color: ${({ theme }) => theme.colors.main};
+  margin-left: 5px;
+  background-color: transparent;
   border: none;
-  border-radius: 4px;
-  color: ${({ theme }) => theme.colors.font};
+  color: ${props => props.theme.text};
   cursor: pointer;
-  transition: all 250ms;
-
-  &:hover,
-  &:focus,
-  &:active {
-    box-shadow: 0 0 6px ${({ theme }) => theme.colors.font};
+  & > svg {
+    transition: all 0.3s;
+  }
+  &:hover:not(:disabled) > svg,
+  &:focus > svg,
+  &:active > svg {
+    color: #9c1111;
+    animation-name: ${animDelete};
+    animation-duration: 1s;
+    animation-iteration-count: infinite;
   }
 `;
 const ItemInfo = styled.p`
   display: flex;
   justify-content: space-between;
+  align-items: baseline;
+  font-size: 10px;
+  @media screen and (min-width: 768px) {
+    font-size: 18px;
+  }
 `;
 
-export { List, ListItem, DeleteButton, ItemInfo };
+export { List, ListItem, Number, DeleteButton, ItemInfo, CallTo, MailTo };
